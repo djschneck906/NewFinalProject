8a59a5d64b09d341db4dd7292f7e1f48ca772c03
indolence <ECCone@student.naperville203.org>
2017-05-12 10:24:10 -0500

    mazeNameGenerator

---
+++ mazeNameGenerator.java
@@ -0,0 +1,28 @@
+
+
+/**
+ * Write a description of class mazeNameGenerator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class mazeNameGenerator
+{
+    public static void main(String[] args){
+        int y = 1;
+        for(int x = 1; x <51; x++){
+            System.out.print(" m" + y + "w" + x+",");                 
+    }
+        y++;
+        System.out.println();
+         for(int x = 1; x <51; x++){
+            System.out.print(" m" + y + "w" + x +",");                 
+    }
+        y++;
+        System.out.println();
+    for(int x = 1; x <51; x++){
+            System.out.print(" m" + y + "w" + x+",");                 
+    }
+
+}
+}
